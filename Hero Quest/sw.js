86bdaf4c56445da94d38e5f50c68efa855d7d0a7
Gonçalo Santos <85107663+domios5@users.noreply.github.com>
2026-04-28 16:03:01 +0100

    Update sw.js

--- Hero Quest/sw.js
+++ Hero Quest/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('hero-quest-v1').then((cache) => {
-      return cache.addAll(['index.html', 'manifest.json']);
+      return cache.addAll(['jogo.html', 'manifest.json']);
     })
   );
 });
