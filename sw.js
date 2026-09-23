2d7d0d35afea77b3f3d1b9a877617ffc13bd42a8
Gonçalo Santos <85107663+domios5@users.noreply.github.com>
2026-09-23 10:34:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('hero-quest-v4').then((cache) => {
+    caches.open('hero-quest-v5').then((cache) => {
       return cache.addAll(['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png']);
     })
   );
@@ -9,7 +9,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== 'hero-quest-v4').map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== 'hero-quest-v5').map((k) => caches.delete(k)))
     )
   );
 });
