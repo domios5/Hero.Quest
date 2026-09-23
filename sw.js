fd8964d999233887366d950d60c2038f0b00b017
Gonçalo Santos <85107663+domios5@users.noreply.github.com>
2026-09-23 12:16:58 +0100

    version 1.0.0.2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('hero-quest-v8').then((cache) => {
+    caches.open('hero-quest-v9').then((cache) => {
       return cache.addAll(['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png']);
     })
   );
@@ -9,7 +9,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== 'hero-quest-v8').map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== 'hero-quest-v9').map((k) => caches.delete(k)))
     )
   );
 });
